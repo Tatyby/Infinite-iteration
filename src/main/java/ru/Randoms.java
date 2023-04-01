1c74b894c252634d93768e220475d7aaa02935c7
Tatiana <in_sdo@mail.ru>
2023-04-01 19:45:30 +0300

    adjustment

--- src/main/java/ru/Randoms.java
+++ src/main/java/ru/Randoms.java
@@ -1,8 +1,6 @@
 package ru;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 
@@ -14,46 +12,22 @@ public class Randoms implements Iterable<Integer> {
     public Randoms(int min, int max) {
         this.max = max;
         this.min = min;
-    }
-
-    public List<Integer> listRandom() {
-
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < (max - min); i++) {
-            list.add(generateRandom());
-        }
-        return list;
+        this.random = new Random();
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
-            int index;
-
             @Override
             public boolean hasNext() {
-                if (index < listRandom().size()) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return true;
             }
 
             @Override
-            public Integer next() throws IndexOutOfBoundsException {
-                try {
-                    index++;
-                    return listRandom().get(index);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new IndexOutOfBoundsException("Такого числа нет");
-                }
+            public Integer next() {
+                return random.nextInt((max - min) + 1) + min;
             }
         };
     }
-
-    public int generateRandom() {
-        Random r = new Random();
-        return r.nextInt((max - min) + 1) + min;
-    }
-
 }
+
